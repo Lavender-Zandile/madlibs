c04efa522d2a00ca182392c0ba5f454c8c9bbf7f
Lavender Tshuma <53860302+Lavender-Zandile@users.noreply.github.com>
2022-04-13 11:51:51 +0200

    Add files via upload

---
+++ madlibs/file.py
@@ -0,0 +1,8 @@
+#Madlibs, it should prompt user input. Exercise focuses on string concaternation using F string
+name = input("Enter your full name ")
+age = input("Enter your age in years ")
+activity = input("Write what you love to do ")
+careerpath =input("Enter your career of choice ")
+
+print(f"Hie my name is {name}. I am {age}.I love to {activity}.I see my self in the next five years as a {careerpath}")
+ 
